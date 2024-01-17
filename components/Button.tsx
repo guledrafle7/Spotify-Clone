aadcfb286c3a20d7c20952b4fa5e40603e5eaa27
guledrafle7 <guledrafle7@gmail.com>
2024-01-17 16:05:24 +0000

    Created the button component allowing me to further advance my layout

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,9 +1,24 @@
-const Button = () => {
-    return ( 
-        <div>
-            Button
-        </div> 
-     );
-}
+import { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLBodyElement> {}
+
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
+    className,
+    children,
+    disabled,
+    type = "button",
+    ...props
+}, ref) => {
+    return (
+        <button type={type} className={twMerge('w-full rounded-full bg-green-500')}>
+            {children}
+        </button>
+    )
+})
+
+Button.displayName = "Button";
+
 
 export default Button;
