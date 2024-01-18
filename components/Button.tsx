8ea21ae1cbb0ca7d59e1986050e5f4a50e8a6c14
guledrafle7 <guledrafle7@gmail.com>
2024-01-18 13:57:49 +0000

    Redesigned and updated button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,7 @@
 import { forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLBodyElement> {}
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
@@ -12,7 +12,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
     ...props
 }, ref) => {
     return (
-        <button type={type} className={twMerge('w-full rounded-full bg-green-500')}>
+        <button type={type} className={twMerge('w-full rounded-full bg-green-500 border border-transparent px-3 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-black font-bold hover:opacity-75 transition', className)} disabled={disabled} ref={ref} {...props} >
             {children}
         </button>
     )
