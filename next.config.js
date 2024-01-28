75794362f4d4c97162625706557955a9819bade7
guledrafle7 <guledrafle7@gmail.com>
2024-01-28 19:11:35 +0000

    Updated action files and packages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['yxqcelsijidryrovanod.supabase.co'],
+      }
+}
 
 module.exports = nextConfig
